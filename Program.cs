e3797c205b9a13233f85c4e465a7fc91dc7d93e7
alexander <tyjbox@gmail.com>
2022-07-01 13:58:47 +0700

    Solving the task in the language C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array = new string[] {"hello","world","555","1984","587","1489"};
+
+for(int i = 0; i < array.Length; i++)
+    if(array[i].Length <=3)
+    Console.Write(array[i]+ "," );
+ 
+
+
+
+
+
+
+
+
+
+
